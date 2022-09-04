530995f27aed38bf71a582543d341f35aac5fa1a
AtsKotzen <atsaudio@gmail.com>
2022-09-04 19:49:50 -0300

    Ajustes de layout

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,5 @@
-import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
-import React, { Component } from 'react';
+import {Text, View, StyleSheet, TextInput, Button} from 'react-native';
+import React, {Component} from 'react';
 import HathorHeader from '../components/HathorHeader';
 
 export default class Login extends Component {
@@ -7,29 +7,32 @@ export default class Login extends Component {
     return (
       <View style={styles.container}>
         <HathorHeader withLogo />
-        <TextInput
-          style={styles.textInput}          
-          placeholder='E-mail ou Celular'
-          placeholderTextColor='white'
-          underlineColorAndroid='white'         
-        />
-        <TextInput
-          style={styles.textInput}          
-          placeholder='Senha'
-          placeholderTextColor='white'
-          underlineColorAndroid='white'         
-        />
-        <Button
-        style={styles.btn}       
-        color={'#fda800'}        
-        title={`Login`}
-        onPress={() => this.props.navigation.navigate('MainScreen')}
-        />
-        <Text style={styles.text}>Ainda não tem uma conta?</Text>
-        <Text style={styles.link}>Cadastre-se</Text>
-        <Text style={styles.info}>profito.club</Text>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="E-mail ou Celular"
+            placeholderTextColor="white"
+            underlineColorAndroid="white"
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Senha"
+            placeholderTextColor="white"
+            underlineColorAndroid="white"
+          />
+        </View>
+        <View style={styles.buttonView}>
+          <Button
+            color={'#fda800'}
+            title={`Login`}
+            onPress={() => this.props.navigation.navigate('MainScreen')}
+          />
+          <Text style={styles.text}>Ainda não tem uma conta?</Text>
+          <Text style={styles.link}>Cadastre-se</Text>
+          <Text style={styles.profitoClub}>profito.club</Text>
+        </View>
       </View>
-    )
+    );
   }
 }
 
@@ -37,32 +40,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#232323',
-    paddingTop: 50     
-  }, 
+    paddingTop: 50,
+  },
+  inputView: {
+    paddingTop: 45,
+  },
   textInput: {
     width: 200,
     color: 'white',
-    alignSelf: 'center' 
+    alignSelf: 'center',
   },
   text: {
     color: 'white',
     alignSelf: 'center',
-    paddingTop: 10
+    paddingTop: 10,
   },
-  info: {
+  profitoClub: {
     color: 'white',
     fontSize: 21,
     fontWeight: 'bold',
-    alignSelf: 'center',
-    opacity: 30,
-    paddingTop: 15
+    alignSelf: 'center',    
+    paddingTop: 30,
   },
   link: {
     color: '#fda800',
-    alignSelf: 'center'    
-  },
-  btn: {
     alignSelf: 'center',
-    paddingTop: 10   
-  }
-})
+  },
+  buttonView: {
+    alignSelf: 'center',
+    paddingTop: 5,
+    width: 200
+  },
+});
